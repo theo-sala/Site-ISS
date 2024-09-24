e6505a48dffd27912cdf5c08ea5cb0b827ff692c
theo-sala <sala.theo@outlook.fr>
2024-09-24 11:58:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function reactionAuclic()
+{
+    window.alert("Au survol de certaines images un point d'interrogation apparaît, si vous laissez la souris immobile, une information apparaîtra.");
+    window.alert("Certains liens sont intégrés à la page, ils sont signalés par un texte souligné ou par la couleur rouge au survol.");
+}
